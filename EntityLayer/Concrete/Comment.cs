41dcb9057513ad73b183a5015d002d58d02be858
ckmkomer <101353123+ckmkomer@users.noreply.github.com>
2023-07-22 13:07:51 +0300

    Class
    Contact,ContactUs,Comment

---
+++ EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Comment
+    {
+        
+        public int CommentID { get; set; }
+
+        public int UserID { get; set; }
+        public string CommentText { get; set; }
+       
+        public string StarCount { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+  public class Contact
+    {
+     
+        public int ContactID { get; set; }
+        public string Adress { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string OpeningHour { get; set; }
+        public string İframe{ get; set;}
+    }
+}
