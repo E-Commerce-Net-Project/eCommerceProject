392fa145e586e613deab152194af8c52fb4bbb75
Efrun Evdi <efruncetkin@gmail.com>
2023-07-23 21:23:01 +0300

    DAL Generic işlemleri tamamlandı.

---
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly Context _context;
+
+        public GenericRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void Delete(T t)
+        {
+            _context.Remove(t);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetByFilter(Expression<Func<T, bool>> filter)
+        {
+            return _context.Set<T>().Where(filter).ToList();
+        }
+
+        public T GetByID(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            _context.Add(t);
+            _context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            _context.Update(t);
+            _context.SaveChanges();
+        }
+    }
+}
