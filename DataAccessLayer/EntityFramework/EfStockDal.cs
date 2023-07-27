4f9a40b392cc35990f7e5873339cea985f3140cb
ckmkomer <101353123+ckmkomer@users.noreply.github.com>
2023-07-27 10:33:17 +0300

    SocialMedia,Sponsor,Stock

---
+++ DataAccessLayer/EntityFramework/EfStockDal.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfStockDal : GenericRepository<Stock>, IStockDal
+    {
+        public EfStockDal(Context context) : base(context)
+        {
+        }
+    }
+}
