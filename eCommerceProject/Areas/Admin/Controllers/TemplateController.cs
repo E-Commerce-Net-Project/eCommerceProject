c2783d1abe94046f705968a987a0805821a53951
Efrun Evdi <efruncetkin@gmail.com>
2023-08-05 00:47:06 +0300

    Admin Layout'u Partiallarıyla beraber tamamlandı.

---
+++ eCommerceProject/Areas/Admin/Controllers/TemplateController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TemplateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HeaderPartial()
+        {
+            return View();
+        }
+
+        public IActionResult LoaderPartial()
+        {
+            return View();
+        }
+
+        public IActionResult NavbarPartial()
+        {
+            return View();
+        }
+
+        public IActionResult RightbarPartial() // Mesajlar Bildirimler Mesaj Yazma
+        {
+            return View();
+        }
+
+        public IActionResult SidebarPartial()
+        {
+            return View();
+        }
+
+        public IActionResult ScriptPartial()
+        {
+            return View();
+        }
+    }
+}
