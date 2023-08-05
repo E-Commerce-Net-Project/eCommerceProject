b6975ada2ab2c5cbe2fc428d70fb8cebb6046fdd
HakkiDogan <180316029@ogr.cbu.edu.tr>
2023-08-05 12:49:11 +0300

    Stock,SubCategory,ContactUs,Tag ve Wishlist Dto sınıfları eklendi.

---
+++ DtoLayer/Dtos/TagDto/ResultTagDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoLayer.Dtos.TagDto
+{
+    public class ResultTagDto
+    {
+        public int TagID { get; set; }
+        public string Name { get; set; }
+        public bool? IsActive { get; set; }
+        public bool? IsDeleted { get; set; }
+    }
+}
